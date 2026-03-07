c5673dfd96141f1dda3ea4ea7febb38583f06b98
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-07 22:41:35 +0100

    Renamed ExampleMixin.java to PreventPickingUp.java

--- src/main/java/ohne/name/mixin/PreventPickingUp.java
+++ src/main/java/ohne/name/mixin/PreventPickingUp.java
@@ -1,7 +1,6 @@
 package ohne.name.mixin;
 
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import ohne.name.TotemPlayerHandle;
@@ -11,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
-public class ExampleMixin {
+public class PreventPickingUp {
 	@Inject(method = "playerTouch", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;getItem()Lnet/minecraft/world/item/ItemStack;"), cancellable = true)
 	private void init(Player player, CallbackInfo ci) {
 		if(player instanceof ServerPlayer serverPlayer) {
