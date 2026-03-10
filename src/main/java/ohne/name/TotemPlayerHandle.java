e9c4c1eceb4be7ac76ba0279c02a38309a925461
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-10 01:04:17 +0100

    Removed useless imports (TotemPlayerHandle))

--- src/main/java/ohne/name/TotemPlayerHandle.java
+++ src/main/java/ohne/name/TotemPlayerHandle.java
@@ -13,8 +13,6 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.CopperChestBlock;
-import net.minecraft.world.level.block.WeatheringCopperBlocks;
 import net.minecraft.world.level.storage.LevelData;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
