906531a062ccbfd00a65a86f23d2298aa6fcd6bd
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-11 00:28:02 +0100

    Made the Creative Respawner Indestructible

--- src/main/java/ohne/name/TotemBlocks.java
+++ src/main/java/ohne/name/TotemBlocks.java
@@ -26,7 +26,7 @@ public class TotemBlocks {
     public static final Block CREATIVE_RESPAWNER = register(
             "creative_respawner",
             properties -> new TotemRespawner(properties, false),
-            BlockBehaviour.Properties.of().sound(SoundType.CREAKING_HEART).friction(0.999F).jumpFactor(0F).mapColor(MapColor.SNOW).strength(50f, 1200f),
+            BlockBehaviour.Properties.of().sound(SoundType.CREAKING_HEART).friction(0.999F).jumpFactor(0F).mapColor(MapColor.SNOW).strength(-1.0F, 3600000.0F),
             true
     );
 
