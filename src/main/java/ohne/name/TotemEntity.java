57bf6c5e4b379d6b6d0ea5653a6c10730ea189d2
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-14 18:59:01 +0100

    Changed: Hardcoded that the TotemEntity can not be damaged by Explosions

--- src/main/java/ohne/name/TotemEntity.java
+++ src/main/java/ohne/name/TotemEntity.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.Level;
 import org.jspecify.annotations.NonNull;
 
 public class TotemEntity extends ItemEntity {
-    boolean isImmuneToExplosion = true;
 
     public TotemEntity(EntityType<? extends ItemEntity> entityType, Level world) {
         super(entityType, world);
@@ -24,6 +23,6 @@ public class TotemEntity extends ItemEntity {
 
     @Override
     public boolean ignoreExplosion(@NonNull Explosion explosion) {
-        return this.isImmuneToExplosion;
+        return true;
     }
 }
