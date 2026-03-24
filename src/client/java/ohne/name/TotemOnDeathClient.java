d1916e7b2df2409cb67c48ad10181cfac3c79147
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-24 02:01:13 +0100

    Changed Place where Render type is given to the block/models

--- src/client/java/ohne/name/TotemOnDeathClient.java
+++ src/client/java/ohne/name/TotemOnDeathClient.java
@@ -1,12 +1,10 @@
 package ohne.name;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
-import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 
 public class TotemOnDeathClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		BlockRenderLayerMap.putBlock(TotemBlocks.RESPAWNER, ChunkSectionLayer.TRANSLUCENT);
+		//BlockRenderLayerMap.putBlock(TotemBlocks.RESPAWNER, ChunkSectionLayer.TRANSLUCENT);
 	}
 }
