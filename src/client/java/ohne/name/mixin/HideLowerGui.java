ef9799bc86e02fe04922eae9f532492c00dadb5a
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-26 22:19:34 +0100

    Added a client mixin that hides the Hotbar when dead

---
+++ src/client/java/ohne/name/mixin/HideLowerGui.java
@@ -0,0 +1,21 @@
+package ohne.name.mixin;
+
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import ohne.name.TotemOnDeathClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Gui.class)
+public class HideLowerGui {
+
+    @Inject(method = "extractHotbarAndDecorations", at = @At("HEAD"), cancellable = true)
+    public void HideGui(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        if(TotemOnDeathClient.IsDead) {
+            ci.cancel();
+        }
+    }
+}
