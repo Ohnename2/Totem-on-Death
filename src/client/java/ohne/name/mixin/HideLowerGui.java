d4397dc2503f0f3157af2b03f6ba81c7dbd9427f
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-26 22:31:34 +0100

    Refactored classes in HideLowerGui

--- src/client/java/ohne/name/mixin/HideLowerGui.java
+++ src/client/java/ohne/name/mixin/HideLowerGui.java
@@ -2,7 +2,7 @@ package ohne.name.mixin;
 
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.GuiGraphics;
 import ohne.name.TotemOnDeathClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Gui.class)
 public class HideLowerGui {
 
-    @Inject(method = "extractHotbarAndDecorations", at = @At("HEAD"), cancellable = true)
-    public void HideGui(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+    @Inject(method = "renderHotbarAndDecorations", at = @At("HEAD"), cancellable = true)
+    public void HideGui(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
         if(TotemOnDeathClient.IsDead) {
             ci.cancel();
         }
