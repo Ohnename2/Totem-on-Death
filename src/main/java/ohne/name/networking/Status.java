651eb22f611d7f21c5f7aea163375fd398a2c5cb
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-03-27 18:15:41 +0100

    Changed the Packet status_update now sends an Array of all Dead players + now all dead Players should be fully Invisible.

--- src/main/java/ohne/name/networking/Status.java
+++ src/main/java/ohne/name/networking/Status.java
@@ -1,16 +1,21 @@
 package ohne.name.networking;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.util.collection.ByteObjectMap;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
 import ohne.name.TotemOnDeath;
+import java.util.List;
+import java.util.UUID;
 
-public record Status(boolean IsDead) implements CustomPacketPayload{
+public record Status(List<UUID> DeadPlayers) implements CustomPacketPayload {
     public static final Identifier STATUS_UPDATE_ID = Identifier.fromNamespaceAndPath(TotemOnDeath.MOD_ID, "status_update");
     public static final CustomPacketPayload.Type<Status> ID = new CustomPacketPayload.Type<>(STATUS_UPDATE_ID);
-    public static final StreamCodec<RegistryFriendlyByteBuf, Status> CODEC = StreamCodec.composite(ByteBufCodecs.BOOL, Status::IsDead, Status::new);
+    public static final StreamCodec<RegistryFriendlyByteBuf, Status> CODEC = StreamCodec.composite(UUIDUtil.STREAM_CODEC.apply(ByteBufCodecs.list()), Status::DeadPlayers, Status::new);
 
     @Override
     public Type<? extends CustomPacketPayload> type() {
