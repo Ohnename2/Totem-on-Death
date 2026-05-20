fcf6c6b67b329ad809343528d15dfa97135ac84f
Ohnename <90216436+Ohnename2@users.noreply.github.com>
2026-05-20 19:11:42 +0200

    removed not used imports

--- src/main/java/ohne/name/networking/ItemRemoveAmount.java
+++ src/main/java/ohne/name/networking/ItemRemoveAmount.java
@@ -1,16 +1,11 @@
 package ohne.name.networking;
 
-import com.mojang.serialization.Codec;
-import net.fabricmc.fabric.api.networking.v1.FriendlyByteBufs;
-import net.minecraft.core.UUIDUtil;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
 import ohne.name.TotemOnDeath;
-import java.util.List;
-import java.util.UUID;
 
 public record ItemRemoveAmount(int amount) implements CustomPacketPayload {
     public static final Identifier STATUS_UPDATE_ID = Identifier.fromNamespaceAndPath(TotemOnDeath.MOD_ID, "item_remove_amount");
